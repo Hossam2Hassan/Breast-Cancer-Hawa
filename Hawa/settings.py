0b61f6c8082c06fbfc02e176fcd9f48bef35c6e3
7lawa91 <ksalah647@gmail.com>
2022-06-17 17:52:53 +0200

    admin

--- Hawa/settings.py
+++ Hawa/settings.py
@@ -34,7 +34,7 @@ AUTH_USER_MODEL='authentication.User'
 
 INSTALLED_APPS = [
     # 'admin_volt.apps.AdminVoltConfig',
-    'grappelli',
+    # 'grappelli',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- authentication/admin.py
+++ authentication/admin.py
@@ -8,6 +8,9 @@ from .models import User
 #     list_display = ['username', 'email', 'auth_provider', 'created_at','first_name']
 # admin.site.register(User, UserAdmin)
 
+admin.site.site_header = 'Hawa admin'
+admin.site.index_title = 'Database Tables'                 
+admin.site.site_title = 'Hawa adminsitration'
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
